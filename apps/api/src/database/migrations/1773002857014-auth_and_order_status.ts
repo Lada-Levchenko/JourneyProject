b0fcf61d7f70ed937e4ad6cfab507f68df6eea2f
Lada Levchenko <dreamsbreakfast.lio@gmail.com>
2026-03-13 00:19:38 +0200

    fix: removed redundant parts of migration

--- apps/api/src/database/migrations/1773002857014-auth_and_order_status.ts
+++ apps/api/src/database/migrations/1773002857014-auth_and_order_status.ts
@@ -14,41 +14,9 @@ export class AuthAndOrderStatus1773002857014 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "users" ADD "globalRole" "public"."global_role_enum" NOT NULL DEFAULT 'user'`,
     );
-    await queryRunner.query(
-      `ALTER TYPE "public"."orders_status_enum" RENAME TO "orders_status_enum_old"`,
-    );
-    await queryRunner.query(
-      `CREATE TYPE "public"."orders_status_enum" AS ENUM('CREATED', 'PAID', 'CANCELLED', 'COMPLETED')`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" DROP DEFAULT`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" TYPE "public"."orders_status_enum" USING "status"::"text"::"public"."orders_status_enum"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" SET DEFAULT 'CREATED'`,
-    );
-    await queryRunner.query(`DROP TYPE "public"."orders_status_enum_old"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TYPE "public"."orders_status_enum_old" AS ENUM('CREATED', 'PAID', 'CANCELLED')`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" DROP DEFAULT`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" TYPE "public"."orders_status_enum_old" USING "status"::"text"::"public"."orders_status_enum_old"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ALTER COLUMN "status" SET DEFAULT 'CREATED'`,
-    );
-    await queryRunner.query(`DROP TYPE "public"."orders_status_enum"`);
-    await queryRunner.query(
-      `ALTER TYPE "public"."orders_status_enum_old" RENAME TO "orders_status_enum"`,
-    );
     await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "globalRole"`);
     await queryRunner.query(`DROP TYPE "public"."global_role_enum"`);
     await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password_hash"`);
