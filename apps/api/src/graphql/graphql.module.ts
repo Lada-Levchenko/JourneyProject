3738ccb90ef7698ca777ba99a9855843f05c4677
Lada Levchenko <dreamsbreakfast.lio@gmail.com>
2026-03-13 02:10:01 +0200

    feat: docker compose for dev, prod, distroless, postgres and tools

--- apps/api/src/graphql/graphql.module.ts
+++ apps/api/src/graphql/graphql.module.ts
@@ -2,17 +2,17 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { join } from "path";
-import { AppResolver } from "./app.resolver";
+
+const isProd = process.env.NODE_ENV === "production";
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
-      playground: true,
+      autoSchemaFile: isProd || join(process.cwd(), "schema.gql"),
+      playground: !isProd,
       sortSchema: true,
     }),
   ],
-  providers: [AppResolver],
 })
 export class GqlConfigModule {}
